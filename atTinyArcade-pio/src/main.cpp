7ea208cc507083abdad2fbabaf8e1f0641f8663d
jonsag <jonsagebrand@gmail.com>
2022-07-11 00:55:37 +0200

    many games moved to pio

--- atTinyArcade-pio/src/main.cpp
+++ atTinyArcade-pio/src/main.cpp
@@ -1,8 +1,16 @@
 #include <Arduino.h>
 
-//#define TetrisGold
+#define TetrisGold
 //#define Pacman
-#define BatBonanza
+//#define BatBonanza
+//#define Frogger
+//#define SpaceAttack
+//#define Tetris
+//#define TetrisMultiButton
+//#define UFOBreakout
+//#define UFOStacker
+//#define WrenRollercoaster
+//#define Morse // not working, circular function call
 
 #ifdef TetrisGold
 #include <ATtiny-Tetris-Gold.h>
@@ -15,6 +23,46 @@
 #ifdef BatBonanza
 #include <BatBonanzaAttinyArcade.h>
 
+#else
+#ifdef Frogger
+#include <Frogger_Attiny_Arcade.h>
+
+#else
+#ifdef SpaceAttack
+#include <SpaceAttackAttiny.h>
+
+#else
+#ifdef Tetris
+#include <Tetris_Attiny_Arcade.h>
+
+#else
+#ifdef TetrisMultiButton
+#include <Tetris_Multi_Button.h>
+
+#else
+#ifdef UFOBreakout
+#include <UFO_Breakout_Arduino.h>
+
+#else
+#ifdef UFOStacker
+#include <UFO_Stacker_Attiny.h>
+
+#else
+#ifdef WrenRollercoaster
+#include <WrenRollercoasterAttinyArcade.h>
+
+#else
+#ifdef Morse
+#include <MorseAttinyArcade.h>
+
+#endif
+#endif
+#endif
+#endif
+#endif
+#endif
+#endif
+#endif
 #endif
 #endif
 #endif
